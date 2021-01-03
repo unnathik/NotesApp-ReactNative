3741aebeb66b041fc3ee920c735206e48d00bb65
unnathik <unnathiutpal6@gmail.com>
2021-01-03 23:12:51 +0530

    initial commit

---
+++ src/screens/ViewNotes.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import {StyleSheet, View} from 'react-native'
+import { Text, FAB } from 'react-native-paper'
+import AddNotes from './AddNotes'
+import Header from '../component/Header'
+
+function ViewNotes({navigation}){
+    const [notes, setNotes] = useState([])
+
+    const addNotes = note => {
+        note.id = notes.length +1
+        setNotes([...notes, note])
+    }
+
+
+
+    return(
+        <>
+        <Header titleText = 'Note Taking App'/>
+        <View style = {styles.container}>
+            <View style = {styles.titleContainer}>
+                <Text style = {styles.title}>You do not have any notes</Text>
+            </View>
+            <FAB
+            style = {styles.fab}
+            small
+            icon = 'plus'
+            label = 'Add New Note'
+            onPress = {() => navigation.navigate('AddNotes')}
+            />
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+    },
+    title: {
+        fontSize: 20
+    },
+    fab: {
+        backgroundColor: '#219653',
+        position: 'absolute',
+        margin: 20,
+        right: 0,
+        bottom: 10
+    }
+})
+
+export default ViewNotes
